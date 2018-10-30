96f9bcbb14fa6c2c8db1d98d9af1a419a59dab09
Sharon Kynaston <desmogal@gmail.com>
2018-10-30 19:47:14 +0000

    created Top20Container.js

---
+++ src/containers/Top20Container.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Top20Container extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+render(){
+    return(
+    <div>
+        <h2>Top 20 Songs</h2>
+
+    </div>
+    );
+   }
+    
+}
+export default Top20Container;
