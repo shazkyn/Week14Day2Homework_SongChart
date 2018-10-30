a689c747dab3abfe8aa3880fa9cfb86773a1654e
Sharon Kynaston <desmogal@gmail.com>
2018-10-30 20:49:13 +0000

    Song Detail.js created

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,1 +1,9 @@
 import React from 'react'
+
+const SongDetail = () => {
+    return (
+        <h3> Song info</h3>
+    )
+}
+
+export default SongDetail;
--- src/containers/Top20Container.js
+++ src/containers/Top20Container.js
@@ -11,7 +11,7 @@ render(){
     return(
     <div>
         <h2>Top 20 Songs</h2>
-
+        <h3> Song Info</h3>
     </div>
     );
    }
